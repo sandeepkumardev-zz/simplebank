4d384ef9ccabd073d182ee38566d2a13611fc5d7
sandeep kumar <sandeepkumar@192.168.1.3>
2022-01-13 17:09:27 +0530

    style: improved schema

--- db/migration/000001_inir_schema.up.sql
+++ db/migration/000001_inir_schema.up.sql
@@ -5,6 +5,7 @@ CREATE TABLE "accounts" (
   "currency" varchar NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
+CREATE INDEX ON "accounts" ("owner");
 
 CREATE TABLE "entries" (
   "id" bigserial PRIMARY KEY,
@@ -12,6 +13,8 @@ CREATE TABLE "entries" (
   "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
+COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
+CREATE INDEX ON "entries" ("account_id");
 
 CREATE TABLE "transfers" (
   "id" bigserial PRIMARY KEY,
@@ -20,23 +23,11 @@ CREATE TABLE "transfers" (
   "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
-
-ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
-
-ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
-
-ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
-
-CREATE INDEX ON "accounts" ("owner");
-
-CREATE INDEX ON "entries" ("account_id");
-
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
 CREATE INDEX ON "transfers" ("from_account_id");
-
 CREATE INDEX ON "transfers" ("to_account_id");
-
 CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
 
-COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
-
-COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
