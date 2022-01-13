68577e3741d40eb6d3f37502fe47e729a9bad4da
sandeep kumar <sandeepkumar@192.168.1.3>
2022-01-13 16:25:49 +0530

    perf: improve the aaccount query

--- db/query/account.sql
+++ db/query/account.sql
@@ -1,13 +1,20 @@
 -- name: CreateAccount :one
 INSERT INTO accounts (
-  owner, balance, currency
+  owner,
+  balance,
+  currency
 ) VALUES (
   $1, $2, $3
-)RETURNING *;
+) RETURNING *;
 
 -- name: GetAccount :one
 SELECT * FROM accounts
 WHERE id = $1 LIMIT 1;
+
+-- name: GetAccountForUpdate :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1
+FOR NO KEY UPDATE;
 
 -- name: ListAccounts :many
 SELECT * FROM accounts
@@ -16,9 +23,17 @@ LIMIT $1
 OFFSET $2;
 
 -- name: UpdateAccount :one
-UPDATE accounts SET balance = $2
+UPDATE accounts
+SET balance = $2
 WHERE id = $1
 RETURNING *;
 
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
+RETURNING *;
+
 -- name: DeleteAccount :exec
-DELETE FROM accounts WHERE id = $1;
+DELETE FROM accounts
+WHERE id = $1;
